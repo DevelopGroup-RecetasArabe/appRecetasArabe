33d297cc2690f688230bfec5c9da7ac520c77469
gustavocano23 <gustavo00inmare@gmail.com>
2021-03-10 04:17:48 -0600

    Actualizando Repositorio

--- src/components/Forms/SignUp.js
+++ src/components/Forms/SignUp.js
@@ -21,28 +21,23 @@ const SignUp = ({ navigation }) => {
   const [alert, setAlert] = useState(false);
   const [confirmar, setConfit] = useState(false);
 
-  // Verifica que los datos ingresados sean correctos
+  /*Verifica todos los campos dependiendo de la variable de input */
   const handleVerify = (input) => {
     if (input === "fullname") {
-      // Verificar el nombre del usuario
       if (!fullname) setFullnameError(true);
       else setFullnameError(false);
-      //Verificar el usuario
     } else if (input === "username") {
       if (!username) setUsernameError(true);
       else setUsernameError(false);
     } else if (input === "email") {
-      // Verificar el correo electrónico
       if (!email) setEmailError(true);
       else if (!validate(email)) setEmailError(true);
       else setEmailError(false);
     } else if (input === "password") {
-      // Verificar la contraseña
       if (!password) setPasswordError(true);
       else if (password.length < 6) setPasswordError(true);
       else setPasswordError(false);
     } else if (input === "confirmPassword") {
-      // Verificar la confirmación de la contraseña
       if (!confirmPassword) setConfirmPasswordError(true);
       else if (confirmPassword !== password) setConfirmPasswordError(true);
       else setConfirmPasswordError(false);
@@ -56,22 +51,18 @@ const SignUp = ({ navigation }) => {
       .then((response) => {
         setAlert(false);
         setConfit(true);
-        // Obtener el Unique Identifier generado para cada usuario
-        // Firebase -> Authentication
+
         const uid = response.user.uid;
 
-        // Construir el objeto que le enviaremos a la collección de "users"
+        //Mi objeto data
         const data = {
           id: uid,
           fullname,
           username,
           email,
         };
-
-        // Obtener la colección desde Firebase
         const usersRef = firebase.firestore().collection("users");
 
-        // Almacenar la información del usuario que se registra en Firestore
         usersRef
           .doc(uid)
           .set(data)
@@ -88,14 +79,14 @@ const SignUp = ({ navigation }) => {
       .catch((error) => {
         setAlert(true);
         setConfit(false);
-        setError(error.message)
+        setError(error.message);
       });
   };
 
   return (
     <View>
-      {alert ? <Alert type="error" title="Datos Incorrectos" />:null}
-      {confirmar ? <Alert type="success" title="Usuario Creado" />:null}
+      {alert ? <Alert type="error" title="Datos Incorrectos" /> : null}
+      {confirmar ? <Alert type="success" title="Usuario Creado" /> : null}
       <View style={styles.inputs}>
         <InputText
           title="Nombre Completo:"
