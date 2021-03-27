7fe17593eaeed7a646b0c3d6752601c719a3fff9
Carlos Cuadra <cuadracarlos693@gmail.com>
2021-03-26 18:58:09 -0600

    Creando pantalla favoritos

--- src/components/screens/Favorites.js
+++ src/components/screens/Favorites.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 
 const Favorites = () => {
   return (
-    <View>
-      <Text>Favoritos</Text>
-    </View>
+    <ScrollView>
+      <View>
+        <Card
+            title="Kibbe"
+            image={require("../../assets/Kibbe.jpeg")}
+            description="Pastelito de carne picada con cebolla"
+            navigation={navigation}
+          />
+      </View>
+    </ScrollView>
   );
 };
 
