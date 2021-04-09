855c1a4584f06d6facf829019837fc07b81b49ee
gustavocano23 <gustavo00inmare@gmail.com>
2021-04-09 00:16:07 -0600

    Agregando nuevas funciones a mi RecipeContext

--- src/providers/RecipeContext.js
+++ src/providers/RecipeContext.js
@@ -5,19 +5,41 @@ const recipeReducer = (state, action) => {
   switch (action.type) {
     case "errorMessage":
       return { ...state, error: action.payload };
+    case "setCurrentRecipe":
+      return { ...state, currentRecipe: action.payload };
+    case "created":
+      return { ...state, created: action.payload };
     case "createRecipe":
-      return { ...state, recipes: action.payload, created: true };
+      return {
+        ...state,
+        recipes: [...recipes, action.payload],
+        created: true,
+      };
     case "getRecipes":
       return { ...state, recipes: action.payload };
     case "getRecipesByUserID ":
       return { ...state, recipesByUserID: action.payload };
     case "updateRecipe ":
-      return { ...state, recipesByUserID: action.payload };
-    case "deleteRecipe":
       return {
         ...state,
-        recipesByUSerID: action.payload,
-        recipes: action.payload,
+        updated: true,
+        recipesByUserID: state.recipesByUserID.map((recipe) => {
+          if (recipe.id === action.payload.recipe.id) {
+            return {
+              ...recipe,
+              title: action.payload.recipe.title,
+              description: action.payload.recipe.description,
+              arrayIngredients: action.payload.recipe.arrayIngredients,
+              arrayPreparations: action.payload.recipe.arrayPreparations,
+              getImage: action.payload.recipe.getImage,
+            };
+          }
+        }),
+      };
+    case "deleteRecipe":
+      return {
+        state,
+        deleted: true,
       };
     default:
       return state;
@@ -37,8 +59,12 @@ const createRecipe = (dispatch) => async (
   const response = await fetch(image);
   const blob = await response.blob();
 
+  console.log(response);
+  console.log(blob);
+
   /*Proceso para guardar la imagen en firebase storage*/
   let ref = firebase.storage().ref().child(`images/${title}`);
+
   ref
     .put(blob)
     .then(async () => {
@@ -64,9 +90,10 @@ const createRecipe = (dispatch) => async (
         })
         .then(() => {
           dispatch({
-            type: "createRecipe",
+            type: "CreateRecipe",
             payload: "Se creo la nueva receta",
           });
+          dispatch({ type: "created", payload: true });
         })
         .catch((error) => {
           dispatch({
@@ -83,7 +110,7 @@ const createRecipe = (dispatch) => async (
     });
 };
 
-/*Traer los datos de firebase segun el user id*/
+/*Traer los datos de firebase en general*/
 const getRecipes = (dispatch) => async () => {
   let recipes = [];
   const snapshot = await firebase
@@ -99,11 +126,12 @@ const getRecipes = (dispatch) => async () => {
   dispatch({
     type: "getRecipes",
     payload: recipes,
+    confirm: true,
   });
 };
 
 /*Traer los datos de firebase segun el user id*/
-const getRecipesByUserID = (dispatch) => async (userId) => {
+const getRecipesByUserID = (dispatch) => async (userId, updated) => {
   let recipesByUserID = [];
   const snapshot = await firebase
     .firestore()
@@ -130,18 +158,20 @@ const updateRecipes = (dispatch) => (
   arrayPreparations,
   getImage
 ) => {
+  const data = {
+    title,
+    description,
+    arrayIngredients,
+    arrayPreparations,
+    getImage,
+  };
   firebase
     .firestore()
     .collection("recipes")
     .doc(id)
-    .update({
-      title,
-      description,
-      arrayIngredients,
-      arrayPreparations,
-      getImage,
-    })
+    .update(data)
     .then(() => {
+      dispatch({ type: "updateRecipes", payload: { recipe: data } });
       console.log("Recetas Actualizadas");
     });
 };
@@ -160,14 +190,46 @@ const deleteRecipe = (dispatch) => (id) => {
     });
 };
 
+const setCurrentRecipe = (dispatch) => (recipe) => {
+  dispatch({ type: "setCurrentRecipe", payload: recipe });
+};
+const refreshRecipe = (dispatch) => (refresh) => {
+  return !refresh;
+};
+
+const resetCreated = (dispatch) => () => {
+  dispatch({ type: "created", payload: false });
+};
+
 //Exportar las funcionalidades del contexto
 
 export const { Provider, Context } = createDataContext(
   recipeReducer,
-  { createRecipe, getRecipesByUserID, getRecipes, updateRecipes, deleteRecipe },
+  {
+    createRecipe,
+    getRecipesByUserID,
+    getRecipes,
+    updateRecipes,
+    deleteRecipe,
+    setCurrentRecipe,
+    refreshRecipe,
+    resetCreated,
+  },
   {
     recipes: [],
     recipesByUserID: [],
+    refresh: false,
     error: "",
+    currentRecipe: {
+      id: "",
+      title: "",
+      description: "",
+      arrayIngredients: [],
+      arrayPreparations: [],
+      getImage: "",
+    },
+    created: false,
+    updated: false,
+    deleted: false,
   }
 );
